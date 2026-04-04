2e1ae15282a2b6549248a74f16b8104ab47af95b
Tanvir-Alam625 <devtanvir01@gmail.com>
2026-04-04 22:39:17 +0600

    added student and runnable

---
+++ app/src/main/java/exceptions/ExceptionExample.java
@@ -0,0 +1,13 @@
+package student.exceptions;
+
+public class ExceptionExample {
+    
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero!");
+        }
+    }
+}
